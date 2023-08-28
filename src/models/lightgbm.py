63408641742ec4984ac220f79349c3c7d89e4244
abap34 <uchi_ymgc@her0.jp>
2023-08-28 15:21:24 +0900

    log_summaryを使うためにfitからtrainに変更

--- src/models/lightgbm.py
+++ src/models/lightgbm.py
@@ -1,5 +1,6 @@
 from wandb.lightgbm import wandb_callback, log_summary
 import lightgbm as lgb
+import wandb
 
 from model import AbstractModel
 from utils import save_dict, load_dict
@@ -11,33 +12,35 @@ class LightGBMRegressionModel(AbstractModel):
         super().__init__(name, params)
 
     def init_model(self):
-        self.model = lgb.LGBMRegressor(**self.params)
+        pass
 
     def train_fold(self, fold, train_x, train_y, val_x, val_y, params=None):
         fold_name = "fold_{}".format(fold)
         if params is None:
             params = {}
         early_stopping_rounds = params.pop("early_stopping_rounds", 100)
-        params["eval_metric"] = params.pop("eval_metric", "rmse")
-        params["verbose"] = params.pop("verbose", 100)
         early_stop_callback = lgb.early_stopping(early_stopping_rounds)
-        gbm = self.model.fit(
-            train_x,
-            train_y,
-            eval_set=(val_x, val_y),
-            callbacks=[wandb_callback(), early_stop_callback],
-            **params
+        train_data = lgb.Dataset(train_x, train_y)
+        val_data = lgb.Dataset(val_x, val_y)
+        gbm = lgb.train(
+            params,
+            train_data,
+            valid_sets=[train_data, val_data],
+            valid_names=["train", "val"],
+            callbacks=[wandb_callback(), early_stop_callback]
         )
-        val_pred = self.model.predict(val_x)
+        val_pred = gbm.predict(val_x)
         log_summary(gbm, save_model_checkpoint=True)
+        self.model = gbm
         return val_pred
 
     def predict(self, test_x):
         return self.model.predict(test_x)
 
+
     def save_model(self, path):
         self.model.save_model(path)
-        save_dict(path + "_params.yaml", self.model.get_params())
+
 
     def load_model(self, path):
         self.model = lgb.Booster(model_file=path)
